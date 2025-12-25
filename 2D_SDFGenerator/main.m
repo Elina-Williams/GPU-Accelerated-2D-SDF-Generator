8aa719902ce126799d44d6c7428247849ee10a22
elina <apple_elina_6022@outlook.com>
2025-12-25 10:22:57 +0800

    Initial Commit

---
+++ 2D_SDFGenerator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2D_SDFGenerator
+//
+//  Created by Elina Williams on 25/12/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
